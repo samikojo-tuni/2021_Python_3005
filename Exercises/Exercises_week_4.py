aa1bb4d30cc0ad5651dfdf7ed7a3eda342bfa6d5
Sami Kojo-Fry <sami.kojo@tuni.fi>
2021-02-12 11:31:58 +0200

    Funtioharjoituksia

---
+++ Exercises/Exercises_week_4.py
@@ -0,0 +1,50 @@
+'''
+Harjoitus 1
+
+Kirjoita funktio, joka ottaa parametrina määrittämättömän määrän numeroita ja 
+kertoo ne yhteen. Funktio palauttaa tuloksen paluuarvona.
+'''
+
+
+'''
+Harjoitus 2
+
+Kirjoita funktio, joka palauttaa suurimman luvun, mikä on tallennettu parametrina
+saatuun tietorakenteeseen. Mieti myös sopiva arvo palautettavaksi siinä tapauksessa, että 
+tietorakenne ei sisällä mitään lukuja.
+Toteuta myös vastaava funktio, mikä palauttaa pienimmän luvun tietorakenteessa.
+'''
+
+
+'''
+Harjoitus 3
+
+Kirjoita funktio, joka laskee parametrina saadun sanan vokaalisen määrän. Vokaaleja
+ovat kirjaimet [a, e, i, o, u, y, ä, ö] (ts. suomen kielen mukaiset vokaalit :)
+'''
+
+
+'''
+Harjoitus 4
+
+Kirjoita funktio, joka laskee parametrina välitetyn positiivisen kokonaisluvun
+kertoman. Toteuta myös virheiden käsittely.
+
+Määritelmä: "Positiivisen kokonaisluvun n kertoma on luvun n ja kaikkien sitä pienempien 
+positiivisten kokonaislukujen tulo, ja se merkitään n!."
+(Wikipedia, https://fi.wikipedia.org/wiki/Kertoma)
+ 
+Nollan kertomaksi on sovittu 1.
+'''
+
+
+'''
+Harjoitus 5
+
+Palindromi
+Kirjoita funktio, joka tarkistaa, onko sana palindromi. Palindromi on sana, joka on sama
+kirjoitettuna oikein ja väärin päin. Sanan kirjainten koolla ei ole merkitystä.
+Oletus: Sana ei sisällä white space -merkkejä (rivinvaihto, välilyönti, tabulaattori).
+Bonus: Sana saa sisältää white space -merkkejä, mutta niitä ei oteta huomioon tarkistettaessa,
+onko sana palindromi.
+'''
