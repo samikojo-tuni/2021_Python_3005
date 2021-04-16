89712c894d6500336d52d8a6a5ed48686073a95a
Sami Kojo-Fry <sami.kojo@tuni.fi>
2021-04-16 14:23:14 +0300

    FizzBuzz-tehtävä

---
+++ Exercises/tyohaastattelu.py
@@ -0,0 +1,18 @@
+'''
+Seuraavaa tehtävää on käytetty yleisesti työhaastatteluissa ennakkotehtävänä:
+
+Kirjoita funktio, joka ottaa parametrinaan numeron ja palauttaa joko
+"Fizz", "Buzz" tai "FizzBuzz" seuraavien ehtojen perusteella: 
+- Jos numero on jaollinen kolmella, funktio palauttaa "Fizz"
+- Jos numero on jaollinen viidellä, funktio palauttaa "Buzz"
+- Jos numero on jaollinen sekä kolmella että viidellä, funktio palauttaa "FizzBuzz"
+- Jos numero ei ole jaollinen kolmella eikä viidellä, funktio palauttaa numeron itsensä string-muodossa.
+
+Kiinnitä huomiota ratkaisun selkeyteen ja ylläpidettävyyteen.
+
+Esimerkkiajoja:
+fizz_buzz(3) ➞ "Fizz"
+fizz_buzz(5) ➞ "Buzz"
+fizz_buzz(15) ➞ "FizzBuzz"
+fizz_buzz(4) ➞ "4"
+'''
