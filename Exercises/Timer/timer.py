68f4aa2423ad6f6f8fb88d9d9f49b44c8a49cbd5
Sami Kojo-Fry <sami.kojo@tuni.fi>
2021-04-22 13:42:22 +0300

    Timer-harjoitus

---
+++ Exercises/Timer/timer.py
@@ -0,0 +1,8 @@
+# Kirjoita ajastin.
+# Ajastin toimii siten, että käyttäjä syöttää ohjelmaan tunnit, minuutit ja sekunnit
+# Tämän jälkeen ajastin lähtee laskemaan syötytystä ajasta alaspäin tulostaen uuden
+# ajan sekunnin välein komentokehotteeseen.
+# Ohjelman suoritus päättyy, kun ajastin käy loppuun.
+
+# Käytä viime viikolla tekemäämme Time-luokkaa ajastimen toteuttamiseen.
+# Tutustu myös Pythonin time-moduulin sleep-funktioon.
