c3e443a702c521b51fa2ac1c976d8d6a76cb632a
Sami Kojo-Fry <sami.kojo@tuni.fi>
2021-04-30 12:28:41 +0300

    Harjoitukset 8

---
+++ Exercises/Viikkoharjoitukset_8.py
@@ -0,0 +1,17 @@
+# Harjoitus 1:
+#
+# Kirjoita funktio, joka korottaa numeron x potenssiin n.
+# Funktio ottaa vastaan kaksi parametria, kantaluvun x
+# ja exponentin n. Älä käytä operaattoria ** tai
+# funktiota pow. Ota huomioon myös negatiiviset exponentit.
+#
+# a) Kirjoita funktiosta silmukkaan perustuva versio.
+# b) Kirjoita funktiosta rekursiivinen versio.
+
+# Harjoitus 2:
+# Kirjoita funktio, joka tulostaa kaikki alkuluvut luvusta 2
+# parametrina välitettävään lukuun num asti.
+#
+# "Alkuluku on lukua 1 suurempi luonnollinen luku, joka ei ole
+# jaollinen muilla positiivisilla kokonaisluvuilla kuin yhdellä
+# ja itsellään" (Wikipedia, https://fi.wikipedia.org/wiki/Alkuluku)
